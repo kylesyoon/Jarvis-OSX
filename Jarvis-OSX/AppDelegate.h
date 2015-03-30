01f213038b2e0907fc09b5b719e1fb36012f8159
Kyle Yoon <kyle.yoon@vokalinteractive.com>
2015-03-30 14:58:11 -0500

    Initial Commit

---
+++ Jarvis-OSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jarvis-OSX
+//
+//  Created by Kyle Yoon on 3/30/15.
+//  Copyright (c) 2015 Kyle Yoon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Jarvis-OSX/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Jarvis-OSX
+//
+//  Created by Kyle Yoon on 3/30/15.
+//  Copyright (c) 2015 Kyle Yoon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
